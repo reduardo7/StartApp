5670e23979b3baaef3539d403cada70a6627dae3
shahidj <shahid.j@allshoreresources.com>
2015-03-10 01:30:33 +0500

    StartApp.h code

--- src/ios/StartApp.h
+++ src/ios/StartApp.h
@@ -1,1 +1,13 @@
+//
+//  StartApp.h
+//
+//
+//  Created by ShahidJ on 25/02/2015.
+//
+//
+#import <UIKit/UIKit.h>
+#import <Cordova/CDV.h>
+@interface StartApp : CDVPlugin
 
+- (BOOL)startApp:(CDVInvokedUrlCommand*)command;
+@end
